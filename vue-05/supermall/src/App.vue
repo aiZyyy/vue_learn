69a7520a1c9ca6a54037b03691331d7a455146a6
zhangyu <13100650510@163.com>
2020-06-11 18:17:59 +0800

    feat:添加导航栏

--- vue-05/supermall/src/App.vue
+++ vue-05/supermall/src/App.vue
@@ -1,16 +1,39 @@
 <template>
   <div id="app">
-    
+    <TabBar>
+      <TabBarItem>
+        <img solt="item-icon" src="@/assets/img/tabbar/home.svg" alt />
+        <div slot="item-text">首页</div>
+      </TabBarItem>
+      <TabBarItem>
+        <img solt="item-icon" src="@/assets/img/tabbar/category.svg" alt />
+        <div slot="item-text">分类</div>
+      </TabBarItem>
+      <TabBarItem>
+        <img solt="item-icon" src="@/assets/img/tabbar/cart.svg" alt />
+        <div slot="item-text">购物车</div>
+      </TabBarItem>
+      <TabBarItem>
+        <img solt="item-icon" src="@/assets/img/tabbar/profile.svg" alt />
+        <div slot="item-text">我的</div>
+      </TabBarItem>
+    </TabBar>
   </div>
 </template>
 
 <script>
+import TabBar from "@/components/common/tabbar/TabBar";
+import TabBarItem from "@/components/common/tabbar/TabBarItem";
 export default {
   name: "App",
-  components: {}
+  components: {
+    TabBar,
+    TabBarItem
+  }
 };
 </script>
 
 <style>
- @import "assets/css/base.css";
+/* style里面引入资源需要加入@ */
+@import "assets/css/base.css";
 </style>
---
+++ vue-05/supermall/src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="tab-bar-item">
+      <slot name="item-icon"></slot>
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem"
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+}
+</style>
