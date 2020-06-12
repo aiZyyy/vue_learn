4745e2d1049931ac85d7d4ebd92976cb1652a7a7
zhangyu <13100650510@163.com>
2020-06-12 10:42:36 +0800

    feat:导航路由完成

--- vue-05/supermall/src/components/common/tabbar/TabBarItem.vue
+++ vue-05/supermall/src/components/common/tabbar/TabBarItem.vue
@@ -1,27 +1,58 @@
 <template>
-  <div>
-    <div class="tab-bar-item">
-      <slot name="item-icon"></slot>
-      <slot name="item-text"></slot>
+  <div id="tab-bar-item" @click="itemClick">
+    <div class="item-icon" v-show="!isActive">
+      <slot name="icon"></slot>
+    </div>
+    <div class="item-active-icon" v-show="isActive">
+      <slot name="active-icon"></slot>
+    </div>
+    <div class="item-text" :style="activeStyle">
+      <slot name="text"></slot>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TabBarItem"
+  name: "TabBarItem",
+  props: {
+    link: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.link) !== -1;
+    },
+    activeStyle() {
+      return this.isActive ? { color: "red" } : {};
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.link);
+    }
+  }
 };
 </script>
 
-<style>
-.tab-bar-item {
+<style scoped>
+#tab-bar-item {
   flex: 1;
-  text-align: center;
-  height: 49px;
 }
 
-.tab-bar-item img {
+.item-icon img,
+.item-active-icon img {
   width: 24px;
   height: 24px;
+  margin-top: 5px;
+  vertical-align: middle;
+}
+
+.item-text {
+  font-size: 12px;
+  margin-top: 3px;
+  color: #333;
 }
 </style>
